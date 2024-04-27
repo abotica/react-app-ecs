7530ebbdaba0d2fc25d03747918afab12fd865e1
abotica <andrija.botica1613@icloud.com>
2024-04-27 20:22:48 +0200

    Returned button component to previous state

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 
-function Button({children, onClick, colorTheme}) {
-  // const buttonColor = colorTheme ? colorTheme : 'edit-blue'
-  const buttonColor = "edit-blue"
+function Button({children, onClick}) {
 
   return (
-    <button onClick={onClick} className={`[z-index:1] border-[1px] border-[solid] border-${buttonColor} mx-[5px] my-[0] p-[5px] relative cursor-pointer text-${buttonColor} [transition:color_ease-in-out_0.3s] font-bold hover:text-[white] before:content-[''] before:absolute before:bottom-[0] before:left-[0] before:w-full before:h-[0] before:bg-${buttonColor} before:[transition:height_ease-in-out_0.3s] hover:before:h-full before:[z-index:-1]`}>
+    <button onClick={onClick} className={`[z-index:1] border-[1px] border-[solid] border-edit-blue mx-[5px] my-[0] p-[5px] relative cursor-pointer text-edit-blue [transition:color_ease-in-out_0.3s] font-bold hover:text-[white] before:content-[''] before:absolute before:bottom-[0] before:left-[0] before:w-full before:h-[0] before:bg-edit-blue before:[transition:height_ease-in-out_0.3s] hover:before:h-full before:[z-index:-1]`}>
     {children}
     </button>
   )
