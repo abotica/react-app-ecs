dc6e5c91bca18c5bfe8b6973459109f8aea999be
abotica <andrija.botica1613@icloud.com>
2024-05-05 19:22:07 +0200

    Fixed radio button bug.

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Input({ type, placeholder, handleOnChange, name, value, className}) {
+function Input({ type, placeholder, handleOnChange, name, value, className, checked}) {
     return type === 'textarea' ?
         (
             <textarea value={value} name={name} onChange={handleOnChange} maxLength={250} placeholder={placeholder} className={'w-full border-b-2 border-gray-300 focus:outline-none focus:border-edit-blue p-2 my-4' + ' ' + className} required />)
         :
         
         (
-            <input value={value} name={name} onChange={handleOnChange} type={type} placeholder={placeholder} className={'w-full border-b-2 border-gray-300 focus:outline-none focus:border-edit-blue p-2 my-4 caret-black' + ' ' + className}  required/>
+            <input checked={checked} value={value} name={name} onChange={handleOnChange} type={type} placeholder={placeholder} className={'w-full border-b-2 border-gray-300 focus:outline-none focus:border-edit-blue p-2 my-4 caret-black' + ' ' + className}  required/>
         )
 }
 
